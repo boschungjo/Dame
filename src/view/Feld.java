1e8897b5a764c9a24d74caf609022884330c6643
radonjicl <luka.radonjic@ict-campus.net>
2019-04-24 10:56:25 +0200

    Feld Klasse angefangen

--- src/view/Feld.java
+++ src/view/Feld.java
@@ -1,5 +1,32 @@
 package view;
 
-public class Feld {
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import application.DameApp;
+
+public class Feld extends Rectangle {
+	
+	private Stein stein;
+	
+	public boolean hatStein() {
+		return stein != null;
+	}
+
+	public Stein getStein() {
+		return stein;
+	}
+
+	public void setStein(Stein stein) {
+		this.stein = stein;
+	}
+	
+	public Feld(boolean leicht, int x, int y) {
+		setWidth(DameApp.FELDGROESSE);
+		setHeight(DameApp.FELDGROESSE);
+		
+		relocate(x * DameApp.FELDGROESSE, y * DameApp.FELDGROESSE);
+		setFill(leicht ? Color.valueOf("#994C00") : Color.valueOf("#F5DEB3"));
+	}
+	
 
 }
